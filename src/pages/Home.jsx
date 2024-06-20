88a8d32167d2dd6d484b03d10824e3dcee8ef87f
Alex-juju <santievillegas@gmail.com>
2024-06-20 19:29:32 -0300

    clase-7

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <main>
+
+        <h1>Holme</h1>
+        <Link to="/contacto">Ir a Contacto</Link>
+          <Link to="/productos">Ir a Productos</Link>
+    </main>
+  )
+}
+
+export default Home
